043c8eb7ea0f75fcbcecd5bb8a62100e5a51b2ee
christian figueiredo <developer.figueiredo@gmail.com>
2025-10-27 15:26:20 -0300

    add migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,23 @@
+using Application.Contracts;
+using Infrastructure.Data;
+using Infrastructure.Implementation;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IEmployee, EmployeeRepo>();
 
 var app = builder.Build();
 
---
+++ Infrastructure/Implementation/EmployeeRepo.cs
@@ -0,0 +1,48 @@
+﻿using Application.Contracts;
+using Application.DTOs;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Implementation
+{
+    public class EmployeeRepo : IEmployee
+    {
+        private readonly AppDbContext _context;
+        public EmployeeRepo(AppDbContext context)
+        {
+           _context = context;
+        }
+        public async Task<ServiceResponse> AddAsync(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await SaveChangesAsync();
+            return new ServiceResponse(true, "Employee added successfully.");
+
+        }
+
+        public async Task<ServiceResponse> DeleteAsync(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+                return new ServiceResponse(false, "Employee not found.");
+            _context.Employees.Remove(employee);
+            await SaveChangesAsync();
+            return new ServiceResponse(true, "Employee deleted successfully.");
+        }
+
+        public async Task<List<Employee>> GetAsync() =>  await _context.Employees.AsNoTracking().ToListAsync();        
+
+        public async Task<Employee> GetByIdAsync(int id) =>  await _context.Employees.FindAsync(id);
+
+        public async Task<ServiceResponse> UpdateAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await SaveChangesAsync();
+            return new ServiceResponse(true, "Employee updated successfully.");
+
+        }
+        private async Task SaveChangesAsync() =>  await _context.SaveChangesAsync();
+        
+    }
+}
